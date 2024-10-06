5d5a6beaa9b83ace6b64af5ef09ecd135fcaf428
DevouredXd <mccjhon9@gmail.com>
2024-10-06 23:18:50 +0100

    Update index.js
    Change

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Status changer plugin loaded.");
+// This is a basic status changer plugin
+setInterval(() => {
+    const statuses = [
+        "em",
+        "my",
+        "fav",
+        "poopy🙄",
+    ];
+    const randomStatus = statuses[Math.floor(Math.random() * statuses.length)];
+    console.log(`Changing status to: ${randomStatus}`);
+    // Here you would set the Discord status using Vendetta's API (this is just a placeholder)
+}, 1000);
